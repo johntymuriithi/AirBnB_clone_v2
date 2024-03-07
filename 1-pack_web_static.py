b05396f7ca53b925c6fb9013519e36ad5514ec5a
johntymuriithi <muriithijohn634@gmail.com>
2024-03-08 00:30:16 +0300

    add

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 #Fabric Module to generate .tgz archive from contents of specific folder.
 
+
 from fabric.operations import local
 from datetime import datetime
 
+
 def do_pack():
     #Generates a .tgz archive from the contents of the web_static folder."""
-    local("mkdir -p versions")
 
-    # Generate the name for the archive
+    local("mkdir -p versions")
     now = datetime.now().strftime("%Y%m%d%H%M%S")
     archive_name = "web_static_{}.tgz".format(now)
 
