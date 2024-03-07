9aad7aa9254cc9e7d23f33759d029466632010a4
johntymuriithi <muriithijohn634@gmail.com>
2024-03-08 00:16:49 +0300

    add6

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+from fabric import task
+from fabric.operations import local
+from datetime import datetime
+
+@task
+def do_pack():
+    """Generates a .tgz archive from the contents of the web_static folder."""
+    # Create the versions folder if it doesn't exist
+    local("mkdir -p versions")
+
+    # Generate the name for the archive
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_name = "web_static_{}.tgz".format(now)
+
+    # Create the archive
+    result = local("tar -cvzf versions/{} web_static".format(archive_name), capture=True)
+
+    if result.failed:
+        return None
+    else:
+        return "versions/{}".format(archive_name)
