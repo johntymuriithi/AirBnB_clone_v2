279a3d9aa13a4c583b31ca5edc1c779237af19c6
johntymuriithi <muriithijohn634@gmail.com>
2024-03-08 00:28:36 +0300

    add91

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,17 +4,14 @@
 from fabric.operations import local
 from datetime import datetime
 
-
 def do_pack():
     #Generates a .tgz archive from the contents of the web_static folder."""
-    # Create the versions folder if it doesn't exist
     local("mkdir -p versions")
 
     # Generate the name for the archive
     now = datetime.now().strftime("%Y%m%d%H%M%S")
     archive_name = "web_static_{}.tgz".format(now)
 
-    # Create the archive
     result = local("tar -cvzf versions/{} web_static".format(archive_name), capture=True)
 
     if result.failed:
