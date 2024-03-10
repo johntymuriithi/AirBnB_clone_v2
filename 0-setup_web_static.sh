8f684fc876624549944e661abd417b4028bb2dea
johntymuriithi <muriithijohn634@gmail.com>
2024-03-10 23:16:14 +0300

    enought fo the day

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,7 +6,13 @@ apt-get install -y nginx
 
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
+echo "<html>
+<head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 chown -R ubuntu /data/
@@ -19,7 +25,7 @@ printf %s "server {
     root   /var/www/html;
     index  index.html index.htm;
 
-    location /hbnb_static {
+    location /hbnb_static/ {
         alias /data/web_static/current;
         index index.html index.htm;
     }
